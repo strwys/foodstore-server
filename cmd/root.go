18fa6d2a8864289a26e655c9353f78196f895029
cecepsprd <cecepsprd21@gmail.com>
2022-05-23 00:22:11 +0700

    debug

--- cmd/root.go
+++ cmd/root.go
@@ -68,6 +68,6 @@ func initConfig() {
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
 	} else {
-		viper.SetConfigFile(`.env_example`)
+		fmt.Println(err)
 	}
 }
